2cd5afe853a835fd54f4441ce58e278558cca7a0
Prodi <programer972@gmail.com>
2025-03-12 16:52:16 +0100

    mobile adaptation  about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 import About from "../../components/About/About";
 // import Contact from "../../components/Contact/Contact";
 import Hero from "../../components/Hero/Hero"
-// import Scills from "../../components/Scills/Scills";
+import Scills from "../../components/Scills/Scills";
+
 // import Works from "../../components/Works/Works";
 
 export default function Home() {
@@ -12,8 +13,8 @@ export default function Home() {
        <main>
           <Hero />
           <About />
-          {/* <Scills />
-          <Works />
+          <Scills />
+          {/* <Works />
           <Contact /> */}
        </main>
   );
